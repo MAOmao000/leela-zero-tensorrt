cb65473711cc874d2a6152f59eb3b2c08ed66297
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2025-05-02 20:51:07 +0900

    The default batch size is the number of threads.

--- src/Leela.cpp
+++ src/Leela.cpp
@@ -106,16 +106,8 @@ static void calculate_thread_count_gpu(
         if (vm["batchsize"].as<unsigned int>() > 0) {
             cfg_batch_size = vm["batchsize"].as<unsigned int>();
         } else {
-#ifdef _WIN32
             cfg_batch_size =
                 (cfg_num_threads + (gpu_count * 2) - 1) / gpu_count;
-#else
-            cfg_batch_size =
-                (cfg_num_threads + (gpu_count * 1) - 1) / (gpu_count * 2);
-            if (cfg_batch_size > cfg_num_threads) {
-                cfg_batch_size = std::min(cfg_num_threads / 2, size_t{1});
-            }
-#endif
             // no idea why somebody wants to use threads less than the number of GPUs
             // but should at least prevent crashing
             if (cfg_batch_size == 0) {
@@ -130,26 +122,15 @@ static void calculate_thread_count_gpu(
             if (cfg_max_threads < 4) {
                 cfg_batch_size = 1;
             } else {
-#ifdef _WIN32
                 cfg_batch_size =
                     cfg_num_threads * (cfg_max_threads / 2 - 1) / cfg_max_threads;
-#else
-                cfg_batch_size =
-                    cfg_num_threads * (cfg_max_threads / 2 - 1) / (cfg_max_threads / 2);
-#endif
             }
-
             if (cfg_batch_size == 0) {
                 cfg_batch_size = 1;
             }
         }
-#ifdef _WIN32
         cfg_num_threads =
             std::min(cfg_max_threads, cfg_batch_size * gpu_count * 2);
-#else
-        cfg_num_threads =
-            std::min(cfg_max_threads, cfg_batch_size * gpu_count);
-#endif
     }
     if (cfg_num_threads < cfg_batch_size) {
         printf(
