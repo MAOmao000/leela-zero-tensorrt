672812d66905893740e2664d775a6a016c7c3b96
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2025-06-21 00:02:42 +0900

    Change Leelazero version.

--- src/config.h
+++ src/config.h
@@ -72,7 +72,7 @@ enum class NetworkType {
 
 static constexpr auto PROGRAM_NAME = "Leela Zero(TensorRT ladder detection)";
 static constexpr auto PROGRAM_VERSION_MAJOR = "1";
-static constexpr auto PROGRAM_VERSION_MINOR = "3";
+static constexpr auto PROGRAM_VERSION_MINOR = "4";
 
 static constexpr auto MAX_CPUS = 256;
 
