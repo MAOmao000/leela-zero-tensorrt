cec9b011aa2b8d7f7c2bf3c7b408a709a75ec074
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2025-07-25 19:44:24 +0900

    Version upgrade.

--- src/config.h
+++ src/config.h
@@ -76,7 +76,7 @@ enum class NetworkType {
 static constexpr auto PROGRAM_NAME = "Leela Zero";
 static constexpr auto PROGRAM_VERSION_MAJOR = "2";
 static constexpr auto PROGRAM_VERSION_MINOR = "0";
-static constexpr auto PROGRAM_VERSION_PATCH = "3";
+static constexpr auto PROGRAM_VERSION_PATCH = "4";
 
 static constexpr auto MAX_CPUS = 256;
 
