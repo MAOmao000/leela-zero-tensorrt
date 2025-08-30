b88902113206756af2d79342938588fff7abb71a
MAOmao000 <0869xsjq@jcom.zaq.ne.jp>
2025-08-30 20:19:51 +0900

    Add Copyright.

--- src/OpenCL.h
+++ src/OpenCL.h
@@ -1,6 +1,7 @@
 /*
     This file is part of Leela Zero.
     Copyright (C) 2017-2019 Gian-Carlo Pascutto and contributors
+    Copyright (C) 2025 MAOmao000
 
     Leela Zero is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
